abdb3cdc6d736de42c721b6bd22610f7daf213dc
ashurluhuseyn <ashurluh@gmail.com>
2024-11-23 10:15:52 +0400

    Update index.jsx

--- src/pages/Admin/Services/index.jsx
+++ src/pages/Admin/Services/index.jsx
@@ -65,7 +65,7 @@ const AdminServices = () => {
                 {data.map(data => (
                 <tr key={data.id}>
                   <td className='px-6 py-1 whitespace-nowrap text-sm text-gray border'>
-                    <img src={`http://localhost:5000/uploads/services/${data.image}`} alt="" />
+                    <img src={`https://api.1stgroupacademy.com/uploads/services/${data.image}`} alt="" />
                   </td>
                   <td className='px-6 py-1 whitespace-nowrap text-sm text-gray border'>{data.title}</td>
                   <td className='px-6 py-1 whitespace-nowrap text-sm text-gray border' style={tdCSS}>{data.description}</td>
